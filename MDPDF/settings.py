4cf871bdf7723e9c4b007a1c5ca7625d29c8f893
harshitkandpal <harshitheroh5@gmail.com>
2026-03-24 16:38:46 +0530

    debugging

--- MDPDF/settings.py
+++ MDPDF/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-d93&9-d-92%5nt^w(nhm08uyv5c2c(oua8e$^4nr^5w6zsv5t2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ["mdpdf-ten.vercel.app", ".vercel.app", "mdpdf.onrender.com"]
